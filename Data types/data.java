ff024919a3596c3ef8fa414acd9535ab17e2a74d
Jaithra <jjaithra79@gmail.com>
2025-02-01 21:13:04 +0530

    Data types

---
+++ Data types/data.java
@@ -0,0 +1,24 @@
+class data 
+
+{
+    public static void main(String a[]) {
+        int num1 = 9;
+        byte by = 127;
+        short sh = 558;
+        long l = 5854L;
+        float f = 5.8f;
+        double d = 5.8;
+        char c = 'k';
+        boolean b = true;
+
+        System.out.println("num1: " + num1);
+        System.out.println("byte: " + by);
+        System.out.println("short: " + sh);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + b);
+
+    }
+}
