2db06a6fad4f90841526a72b65958abfa20a31c2
Jaithra <jjaithra79@gmail.com>
2025-02-01 18:09:56 +0530

    Variables

---
+++ Variables/addnum.java
@@ -0,0 +1,11 @@
+
+class addnum 
+{
+    public static void main(String a[]) {
+        System.out.print(3 + 5);
+
+        // to print the values in new line use ln
+        System.out.println(3 + 5);
+        System.out.println(8 + 7);
+    }
+}
---
+++ Variables/declarenum.java
@@ -0,0 +1,24 @@
+class declarenum 
+{
+    public static void main(String a[]) 
+    {
+        //storing value
+
+        int num = 3;
+        System.out.println( num);
+
+        //declaring value
+        
+        int num1 = 3;
+        int num2 = 5;
+        System.out.println(num1+ num2);
+
+        //create another variable to print result
+
+        int num3 = 4;
+        int num4 = 3;
+        int result = num3 + num4;
+        System.out.println(result);
+    }
+    
+}
