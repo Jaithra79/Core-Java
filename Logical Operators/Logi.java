d57fb1c7c9f66cc296ef9f63f8770fee9cd5f8d0
Jaithra <jjaithra79@gmail.com>
2025-02-04 16:15:09 +0530

    Logical Operators

---
+++ Logical Operators/Logi.java
@@ -0,0 +1,24 @@
+class Logi {
+
+    public static void main(String[] args) 
+    
+    {
+        
+        int x = 7;
+        int y = 5;
+        int a = 5;
+        int b = 9;
+
+        boolean result = x > y && a > b; 
+        System.out.println(result);
+
+        // while using not operator
+
+        int p = 5;
+        int q = 9;
+
+        boolean result1 = p > q;
+        System.out.println(!result1);
+    }
+    
+}
