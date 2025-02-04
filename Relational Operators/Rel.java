6859d57b95d3b6f764ae0c6d9adda23bc32323c3
Jaithra <jjaithra79@gmail.com>
2025-02-04 15:56:28 +0530

    Relational Operators

---
+++ Relational Operators/Rel.java
@@ -0,0 +1,53 @@
+ class Rel 
+ {
+    
+    public static void main(String[] args) {
+        
+        int x = 6;
+        int y = 5;
+
+        boolean result = x < y;
+        System.out.println(result);
+
+        // suppose if 2 values are equal
+
+        int a = 6;
+        int b = 6;
+
+        boolean result1 = a>=b;
+        System.out.println(result1);
+
+        // suppose if 2 values are not equal
+
+        int m = 6;
+        int n = 7;
+
+        boolean result2 = m != n;
+        System.out.println(result2);
+
+        // suppose if 2 values are not equal and want to compare
+
+        int p = 6;
+        int q = 7;
+
+        boolean result3 = p == q;
+        System.out.println(result3);
+
+         // suppose if 2 values are equal and want to compare
+
+         int u = 6;
+         int v = 6;
+ 
+         boolean result4 = u == v;
+         System.out.println(result4);
+
+         // suppose u use double values
+
+         double c = 8.8;
+         double d = 9.8;
+
+         boolean result5 = c >= d;
+         System.out.println(result5);
+
+    }
+}
