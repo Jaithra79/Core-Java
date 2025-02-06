cbd41bf7a57496e61ce2e72a3c05fee7e9f08af3
Jaithra <jjaithra79@gmail.com>
2025-02-07 00:47:23 +0530

    Conditions

---
+++ Conditions/Condition.java
@@ -0,0 +1,11 @@
+public class Condition 
+ 
+ {
+    public static void main(String[] args) {
+        
+        int x = 18;
+
+        if (x>10)
+        System.out.println("Hello"); 
+    }
+}
---
+++ Conditions/Condition1.java
@@ -0,0 +1,11 @@
+public class Condition1 {
+
+    public static void main(String[] args) {
+        
+        int a = 7;
+        
+        if (true) // if false wont execute
+        System.out.println("Hello");
+    }
+    
+}
