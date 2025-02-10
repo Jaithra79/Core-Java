8529ee367f4f2c139d3c709b25eddfd98d74ba09
Jaithra <jjaithra79@gmail.com>
2025-02-10 16:01:08 +0530

    Condition

---
+++ Conditions/Condition4.java
@@ -0,0 +1,13 @@
+public class Condition4 {
+
+    public static void main(String[] args){
+
+        int x = 18;
+
+        if (x>10 && x<=20)  // 11 - 20
+        System.out.println("Hello");
+        else
+        System.out.println("Bye");
+    }
+    
+}
