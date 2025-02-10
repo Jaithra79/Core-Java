dfb6a4b89dd314596a5721bc494139c000ae152a
Jaithra <jjaithra79@gmail.com>
2025-02-10 15:51:37 +0530

    Condition

---
+++ Conditions/Condition3.java
@@ -0,0 +1,12 @@
+public class Condition3 {
+    
+    public static void main(String[] args){
+
+        int x = 28;
+
+        if (x>10 && x<=20)  // 11 - 20
+        System.out.println("Hello");
+        
+        System.out.println("Bye");
+    }
+}
