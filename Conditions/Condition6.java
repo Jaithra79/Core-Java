681d9355ef84c57a777329b715b83f8af4825107
Jaithra <jjaithra79@gmail.com>
2025-02-10 16:02:24 +0530

    Conditions

---
+++ Conditions/Condition6.java
@@ -0,0 +1,17 @@
+public class Condition6 {
+
+    public static void main (String[] args)
+    {
+        int x = 8;
+        int y = 7;
+
+        if (x>y)
+        {
+        System.out.println(x);
+        System.out.println("Thank You");
+        }
+        else
+        System.out.println(y);
+    }
+    
+}
