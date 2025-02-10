5957d6bdd2f6e6312dd85462c28f8f34196f5f15
Jaithra <jjaithra79@gmail.com>
2025-02-10 15:50:44 +0530

    Condition

---
+++ Conditions/Condition2.java
@@ -0,0 +1,11 @@
+public class Condition2 {
+
+    public static void main(String[] args){
+
+        int x = 18;
+
+        if (x>10 && x<=20)  // 11 - 20
+        System.out.println("Hello");
+    }
+    
+}
