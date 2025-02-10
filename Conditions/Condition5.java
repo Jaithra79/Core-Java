fe401d0c8b7618988fa70eba570a256985ce89f4
Jaithra <jjaithra79@gmail.com>
2025-02-10 16:01:26 +0530

    Conditions

---
+++ Conditions/Condition5.java
@@ -0,0 +1,14 @@
+public class Condition5 {
+
+    public static void main (String[] args)
+    {
+        int x = 5;
+        int y = 7;
+
+        if (x>y)
+        System.out.println(x);
+        else
+        System.out.println(y);
+    }
+    
+}
