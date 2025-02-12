a336737a9d03500c46a7d6334abb54fd8535fc60
Jaithra <jjaithra79@gmail.com>
2025-02-12 22:35:01 +0530

    Loop

---
+++ Loop/Do.java
@@ -0,0 +1,11 @@
+public class Do {
+    
+    public static void main(String a[] ) {
+        int i = 4;
+
+        do{
+            System.out.println("Hi " + i);
+            i++;
+        } while (i<=4);
+    }
+}
