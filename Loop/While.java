f12f430feeedebcd5986c74edd05d358df7179f6
Jaithra <jjaithra79@gmail.com>
2025-02-12 14:25:11 +0530

    Loop

---
+++ Loop/While.java
@@ -0,0 +1,15 @@
+public class While {
+
+    public static void main(String a []) {
+
+        int i = 1;
+
+        while (true) 
+        { 
+            System.out.println("Hi" + i);
+            i++;
+        }
+        
+    }
+    
+}
---
+++ Loop/While1.java
@@ -0,0 +1,17 @@
+
+
+public class While1 {
+
+    public static void main(String a []) {
+
+        int i = 1;
+
+        while (i<=4) 
+        { 
+            System.out.println("Hi" + i);
+            i++;
+        }
+        
+    }
+    
+}
